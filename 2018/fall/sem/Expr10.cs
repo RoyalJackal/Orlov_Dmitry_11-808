5aab420c0f34398a3cfcfcb1c67ca4d28caa8344
Dmitry <dimaorol@gmail.com>
2018-09-16 22:02:07 +0300

    added Expr 10-11

---
+++ 2018/fall/sem/Expr10.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expr10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            //перебор всех значений
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0  || i % 5 == 0)
+                { sum += i; };
+            }
+            Console.WriteLine("Сумма всех положительных чисел меньше 1000 кратных 3 или 5 равна " + sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2018/fall/sem/Expr11.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expr11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите время");
+            String time = Console.ReadLine();
+            //деление строчки со временем на часы и минуты и присовение соотвествующим переменным
+            String [] parts = time.Split(':',' ','.',',');
+            int hour = int.Parse(parts[0]);
+            //приведение к 12-часовому формату
+            if (hour>12) { hour -= 12; };
+            int minute = int.Parse(parts[1]);
+            //вывод с выбором наименьшего угла
+            if (Math.Abs(30 * hour - 6 * minute) > 180)
+            { Console.WriteLine("Угол от часовой к минутной стрелке равен " + (360 - Math.Abs(30 * hour - 6 * minute))); }
+            else
+            { Console.WriteLine("Угол от часовой к минутной стрелке равен " + (Math.Abs(30 * hour - 6 * minute))); };
+            Console.ReadKey();
+        }
+    }
+}
