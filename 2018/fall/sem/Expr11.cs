b99d5ba40cfd652dfa9a867d4cb07a27cde56621
Dmitry <dimaorol@gmail.com>
2018-09-17 09:55:41 +0300

    modified Expr 11, hour arrow moves with minutes

--- 2018/fall/sem/Expr11.cs
+++ 2018/fall/sem/Expr11.cs
@@ -13,16 +13,16 @@ namespace Expr11
             Console.WriteLine("Введите время");
             String time = Console.ReadLine();
             //деление строчки со временем на часы и минуты и присовение соотвествующим переменным
-            String [] parts = time.Split(':',' ','.',',');
+            String[] parts = time.Split(':', ' ', '.', ',');
             int hour = int.Parse(parts[0]);
             //приведение к 12-часовому формату
-            if (hour>12) { hour -= 12; };
+            if (hour > 12) { hour -= 12; };
             int minute = int.Parse(parts[1]);
             //вывод с выбором наименьшего угла
-            if (Math.Abs(30 * hour - 6 * minute) > 180)
-            { Console.WriteLine("Угол от часовой к минутной стрелке равен " + (360 - Math.Abs(30 * hour - 6 * minute))); }
+            if (Math.Abs(30 * hour + 0.5 * minute - 6 * minute) > 180)
+            { Console.WriteLine("Угол от часовой к минутной стрелке равен " + (360 - Math.Abs(30 * hour + 0.5 * minute - 6 * minute))); }
             else
-            { Console.WriteLine("Угол от часовой к минутной стрелке равен " + (Math.Abs(30 * hour - 6 * minute))); };
+            { Console.WriteLine("Угол от часовой к минутной стрелке равен " + (Math.Abs(30 * hour + 0.5 * minute - 6 * minute))); };
             Console.ReadKey();
         }
     }
