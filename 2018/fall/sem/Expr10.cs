3e94a572d3b109cfbb3b22f488db9d87c9b12fb7
Dmitry <dimaorol@gmail.com>
2018-09-17 09:47:34 +0300

    reworked Expr10, now without cycles

--- 2018/fall/sem/Expr10.cs
+++ 2018/fall/sem/Expr10.cs
@@ -10,15 +10,18 @@ namespace Expr10
     {
         static void Main(string[] args)
         {
-            int sum = 0;
-            //перебор всех значений
-            for (int i = 1; i < 1000; i++)
-            {
-                if (i % 3 == 0  || i % 5 == 0)
-                { sum += i; };
-            }
-            Console.WriteLine("Сумма всех положительных чисел меньше 1000 кратных 3 или 5 равна " + sum);
+            Console.WriteLine("Введите числа для нахождения суммы всех положительных чисел меньше 1000 кратных этим числам");
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int sum = AriphmeticProgressionSum(number1) + AriphmeticProgressionSum(number2) - AriphmeticProgressionSum(number1*number2);
+            Console.WriteLine("Сумма всех положительных чисел меньше 1000 кратных {0} или {1} равна {2}",number1, number2, sum);
             Console.ReadKey();
         }
+        //метод, считывающий сумму элементов арифметической прогрессии меньше 1000
+        static int AriphmeticProgressionSum(int number)
+        {
+            return (2 * number + (999 / number - 1) * number) * (999 / number) / 2;
+        }
+
     }
 }
