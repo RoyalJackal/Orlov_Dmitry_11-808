bbe5e6069311ba961d0e996b793b0b7b2ffd29a6
Dmitry <dimaorol@gmail.com>
2018-09-18 22:44:22 +0300

    added Expr 12-13

---
+++ 2018/fall/sem/Expr12.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expr12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //разделение строчки и распределение переменных
+            String s = Console.ReadLine();
+            String[] parts = s.Split(' ');
+            int h = int.Parse(parts[0]);
+            int t = int.Parse(parts[1]);
+            int v = int.Parse(parts[2]);
+            int x = int.Parse(parts[3]);
+            double min;
+            double max;
+            //вычесление минимального времени
+            if (h - t * x <= 0)
+            { min = 0; }
+            else { min = ((h - t * x) / (v - x)); };
+            //вычесление максимального времени
+            if (h / t > x)
+            { max = t; }
+            else { max = (h - t) / x; };
+            Console.WriteLine("Минимальное время равно {0} Максимальное время равно {1}", min, max);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2018/fall/sem/Expr13.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expr13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //разделение строчки и распределение переменных
+            String s = Console.ReadLine();
+            String[] parts = s.Split(' ');
+            int side = int.Parse(parts[0]);
+            int radius = int.Parse(parts[1]);
+            //круг находится в квадрате
+            if (radius <= side / 2)
+            { Console.WriteLine(Math.PI * radius * radius); };
+            //квадрат находится в круге
+            if (radius >= Math.Sqrt(2) * side / 2)
+            { Console.WriteLine(side * side); };
+            //круг пересекает квадрат
+            if (side / 2 < radius && radius < Math.Sqrt(2) * side / 2)
+            { Console.WriteLine(Math.Round(Math.PI * radius * radius - 4 * (SectorArea(radius, side) - TriangleArea(radius, side)),3)); };
+            Console.ReadKey();
+        }
+        static double SectorArea(int r, int a)
+        {
+            return (180 * r * r * 2 * Math.Acos((double)a / (2 * r))) / 360;
+        }
+        static double TriangleArea(int r, int a)
+        {
+            return Math.Sqrt(r * r - (a * a) / 4) * a / 2;
+        }
+    }
+}
