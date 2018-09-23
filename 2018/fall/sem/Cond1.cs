78d7144b83a9ab69b4079142e803dec95d662025
Dmitry <dimaorol@gmail.com>
2018-09-23 23:08:27 +0300

    added Cond 1-2

---
+++ 2018/fall/sem/Cond1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cond1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string startPosition = Console.ReadLine();
+            string endPosition = Console.ReadLine();
+            bool bishop = (Math.Abs(startPosition[0] - endPosition[0]) == Math.Abs(startPosition[1] - endPosition[1]) &&
+                Math.Abs(startPosition[0] - endPosition[0]) != 0);
+            bool knight = (Math.Abs(startPosition[0] - endPosition[0]) == 2 && Math.Abs(startPosition[1] - endPosition[1]) == 1 ||
+                Math.Abs(startPosition[0] - endPosition[0]) == 1 && Math.Abs(startPosition[1] - endPosition[1]) == 2); 
+            bool rook = (startPosition[0] != endPosition[0] && startPosition[1] == endPosition[1] ||
+                startPosition[0] == endPosition[0] && startPosition[1] != endPosition[1]);
+            bool queen = (Math.Abs(startPosition[0] - endPosition[0]) == Math.Abs(startPosition[1] - endPosition[1]) ||
+                startPosition[0] != endPosition[0] && startPosition[1] == endPosition[1] ||
+                startPosition[0] == endPosition[0] && startPosition[1] != endPosition[1] &&
+                (Math.Abs(startPosition[0] - endPosition[0]) != 0 || Math.Abs(startPosition[1] - endPosition[1]) != 0));
+            bool king = (Math.Abs(startPosition[0] - endPosition[0]) <= 1 && Math.Abs(startPosition[1] - endPosition[1]) <= 1 && 
+                (Math.Abs(startPosition[0] - endPosition[0]) == 1 || Math.Abs(startPosition[1] - endPosition[1]) == 1));
+            Console.WriteLine("Слон {0}, Конь {1}, Ладья {2}, Ферзь {3}, Король {4}", bishop, knight, rook, queen, king);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2018/fall/sem/Cond2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cond2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] parts = Console.ReadLine().Split(' ');
+            int x = Int32.Parse(parts[0]);
+            int y = Int32.Parse(parts[1]);
+            int z = Int32.Parse(parts[2]);
+            int a = Int32.Parse(parts[3]);
+            int b = Int32.Parse(parts[4]);
+            int min = Math.Min(Math.Min(x, y), z);
+            if (min > Math.Min(a, b)) Console.WriteLine("Не пролезет");
+            else
+                if (min == x)
+                    if (Math.Min(y, z) > Math.Max(a, b)) Console.WriteLine("Не пролезет");
+                    else Console.WriteLine("Пролезет");
+                else
+                    if (min == y)
+                        if (Math.Min(x, z) > Math.Max(a, b)) Console.WriteLine("Не пролезет");
+                        else Console.WriteLine("Пролезет");
+                    else
+                        if (Math.Min(x, y) > Math.Max(a, b)) Console.WriteLine("Не пролезет");
+                        else Console.WriteLine("Пролезет");
+            Console.ReadKey();
+        }
+    }
+}
