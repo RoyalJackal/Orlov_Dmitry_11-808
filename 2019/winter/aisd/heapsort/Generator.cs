373b3e123fe9c6211eb929811bad57a534de049e
Dmitry <dimaorol@gmail.com>
2019-03-12 16:23:56 +0300

    added heapsort without a report

---
+++ 2019/winter/aisd/heapsort/Generator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Heapsort
+{
+    public static class Generator
+    {
+        //метод генерации файлов, заполенных случайными значениями
+        public static void Generete()
+        {
+            var rnd = new Random();
+            var lines = new List<string>();
+            for (int i = 1; i <= 100; i++)
+            {
+                //заполняем лист случайными 100*(номер файла) элементами и записываем в файл
+                for (int j = 1; j <= i * 100; j++)
+                    lines.Add(rnd.Next().ToString());
+                File.WriteAllLines(String.Format("data{0}.txt", i), lines);
+                //после очередного файла чистим лист
+                lines.Clear();
+            }
+        }
+    }
+}
