637227f14c06d74fae3d5446dcb5c0642ea90967
Dmitry <dimaorol@gmail.com>
2019-03-26 13:27:18 +0300

    added linq contr

---
+++ 2019/spring/misc/LinqContr/Task2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContrLinq
+{
+    public static class Task2
+    {
+        public static IEnumerable<Tuple<T, T>> Solve<T>(this IEnumerable<T> seq1, IEnumerable<T> seq2, Func<T, T, bool> func)
+        {
+            //для каждой пары применяем функцию
+            foreach (var item1 in seq1)
+                foreach (var item2 in seq2)
+                    if (func(item1, item2))
+                        yield return Tuple.Create(item1, item2);
+        }
+    }
+}
---
+++ 2019/spring/misc/LinqContr/Task3.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContrLinq
+{
+    public static class Task3
+    {
+        public static Tuple<string, int, int>[] Solve(Product[] products, Discount[] discounts, Price[] prices, PurchaseData[] purchases)
+        {
+            return purchases
+                .GroupBy(purchase => Tuple.Create(
+                    //отсеиваем продукты с ненужным артиклес, должен остаться один, берём его страну
+                    products.Where(product => product.Article == purchase.Article).First().Country,
+                    purchase.CustomerData))
+                .Select(group => Tuple.Create(
+                    group.Key.Item1,
+                    group.Key.Item2,
+                    group.Sum(purchase =>
+                    {
+                        //отсеиваем нужную скидку и цену
+                        var purchaseDiscount = discounts.Where(discount => discount.CustomerCode == purchase.CustomerData &&
+                            discount.ShopName == purchase.ShopName).First().Value;
+                        var purchasePrice = prices.Where(price => price.Article == purchase.Article &&
+                            price.ShopName == purchase.ShopName).First().Value;
+                        //считаем по формуле
+                        return purchasePrice - purchasePrice * purchaseDiscount / 100;
+                    })))
+                .OrderBy(tuple => Tuple.Create(tuple.Item1, tuple.Item2))
+                .ToArray();
+        }
+    }
+
+    public class Customer
+    {
+        public readonly int CustomerCode;
+        public readonly int BirthYear;
+        public readonly string LivingLocation;
+
+        public Customer(int code, int birthYear, string livingLocation)
+        {
+            CustomerCode = code;
+            BirthYear = birthYear;
+            LivingLocation = livingLocation;
+        }
+    }
+
+    public class Product
+    {
+        public readonly string Article;
+        public readonly string Category;
+        public readonly string Country;
+
+        public Product(string article, string category, string country)
+        {
+            Article = article;
+            Category = category;
+            Country = country;
+        }
+    }
+
+    public class Discount
+    {
+        public readonly int CustomerCode;
+        public readonly string ShopName;
+        public readonly int Value;
+
+        public Discount(int customerCode, string shopName, int value)
+        {
+            CustomerCode = customerCode;
+            ShopName = shopName;
+            Value = value;
+        }
+    }
+
+    public class Price
+    {
+        public readonly string Article;
+        public readonly string ShopName;
+        public readonly int Value;
+
+        public Price(string article, string shopName, int value)
+        {
+            Article = article;
+            ShopName = shopName;
+            Value = value;
+        }
+    }
+
+    public class PurchaseData
+    {
+        public readonly int CustomerData;
+        public readonly string Article;
+        public readonly string ShopName;
+
+        public PurchaseData(int customerData, string article, string shopName)
+        {
+            CustomerData = customerData;
+            Article = article;
+            ShopName = shopName;
+        }
+    }
+}
